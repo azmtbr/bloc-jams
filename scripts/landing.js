1af1f296a24c8af23cf35dce8c7fd034df9c37bb
Jon Malis <jmalis@live.com>
2015-11-30 20:07:58 -0700

    dom events checkpoint

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,7 @@
-var animatePoints = function () {
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function (points) {
  
-	var points = document.getElementsByClassName('point');
 
 	var revealPoint = function (i) {
 		points[i].style.opacity = 1;
@@ -9,9 +10,25 @@ var animatePoints = function () {
 		points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
 	}
 
-	for(var i = 0; i < points.length; i++)
+	for(var i = 0; i < points.length; i++) {
 		revealPoint(i);
+	}
 
 };
+
+window.onload = function() {
+	// Automatically animate the points on a tall screen where scrolling can't trigger the animation
+	if (window.innerHeight > 950) {
+		animatePoints(pointsArray);
+	}
+	var sellingPoints = document.getElementsByClassName('selling-points')[0];
+	var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+	
+	window.addEventListener('scroll', function(event) {
+		if (document.body.scrollTop >= scrillDistance) {
+			animatePoints(pointsArray);
+		}
+	});
+}
 		  
 
